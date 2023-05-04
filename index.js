609ec6488bc7fd3e7a977f5144b31ab14eed800d
Miles-Koldewyn <miles.koldewyn@gmail.com>
2023-05-03 21:14:25 -0600

    added jquery

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-$(document).ready(function(){
-    $('.rummagepic').hover(function() {
-        $(this).addClass('transition');
-    }, function() {
-        $(this).removeClass('transition');
-    });
-});
+  let mybutton = document.getElementById("scroll-to-top");
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  $("#scroll-to-top").click(function(){
+    $(window).scrollTop(0)
+  });
